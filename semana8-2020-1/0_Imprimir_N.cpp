26837f92e77a80ec27cb50fca466a1393b26ea79
Alexz537 <52678667+Alexz537@users.noreply.github.com>
2019-10-15 20:40:36 -0500

    Create 0_Imprimir_N.cpp

---
+++ semana8-2020-1/0_Imprimir_N.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX 1000000
+
+
+int main()
+{
+    //signed long long int n=0;
+    char cad[MAX];
+
+    cin>>cad;
+    cout<<cad<<endl;
+
+    return 0;
+}
